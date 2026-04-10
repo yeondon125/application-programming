366425aa282c1f1b1304f1450d4e24141fa332d8
yeondon <s25030@gsm.hs.kr>
2026-04-10 14:59:13 +0900

    dd

---
+++ js/script2-4.2.js
@@ -0,0 +1,7 @@
+$(function () {
+  $("*").css("color", "red");
+  $("p").css("text-decoration", "underline");
+  $(".point").css("background-color", "yellow");
+  $("#title").css("color", "green");
+  $("p, button").css("font-size", "20px");
+});
