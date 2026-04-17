869e4fe6d36cab1f4870008d1193c48993a32e22
yeondon <s25030@gsm.hs.kr>
2026-04-17 13:48:28 +0900

    d

--- js/script2-4.4.js
+++ js/script2-4.4.js
@@ -1,1 +1,8 @@
-$(function () {});
+$(function () {
+  $(".list1>li:first-child").css("text-decoration", "underline");
+  $(".list1>li").first().css("background-color", "yellow");
+  $(".list1>li:last-child").css("text-decoration", "underline");
+  $(".list1>li").last().css("background-color", "green");
+  $(".list2>li").eq(0).css("color", "red");
+  $(".list1").find(".sub").css("color", "blue");
+});
